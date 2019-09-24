0ccaf4a50d235eec43aa5052b649536f132c5797
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-24 19:14:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,10 +6,17 @@ new_array = []
 counter=0
 
   while array [counter] do
+<<<<<<< HEAD
   new_array << array[counter] ** 2
    counter += 1
   end 
 
 p new_array
 
+=======
+  array[counter] ** array[counter] << new_array
+   counter += 1
+  end 
+
+>>>>>>> eae12e80e72c6d10147829e382e06ba7d22cb936
 end
